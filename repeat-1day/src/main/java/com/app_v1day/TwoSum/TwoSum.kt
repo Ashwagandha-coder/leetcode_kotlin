1c3177fe3c34c3fbee38d8eef2b351f8d0131756
Danil_M <max.cacz@yandex.ru>
2022-11-30 23:13:35 +0500

    [1day] unfinished

---
+++ repeat-1day/src/main/java/com/app_v1day/TwoSum/TwoSum.kt
@@ -0,0 +1,21 @@
+package com.app_v1day.TwoSum
+
+class TwoSum {
+
+    fun twoSum(nums: IntArray, target: Int) {
+
+        var result = 0
+
+        for(i in nums.size - 2 .. 0) {
+            for(j in i + 1 .. nums.size - 1) {
+
+
+            }
+
+        }
+
+    }
+
+
+
+}
