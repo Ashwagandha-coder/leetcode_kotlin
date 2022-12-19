a00ba5442607b464d2f98c1a1048910e5434daba
Ashawagandha-coder <max.cacz@yandex.ru>
2022-12-19 17:29:15 +0500

    [task] done task palindrome

--- app/src/main/java/com/leetcode_kotlin/DoSome.kt
+++ app/src/main/java/com/leetcode_kotlin/DoSome.kt
@@ -1,10 +1,15 @@
 package com.leetcode_kotlin
 
-import com.leetcode_kotlin.TwoSum.twoSumMain
+import com.leetcode_kotlin.PalindromeNumber.palindromeNumber
+
 
 fun main() {
 
-    twoSumMain(intArrayOf(2,7,11,15),9).forEach { println(it) }
+    //twoSumMain(intArrayOf(2,7,11,15),9).forEach { println(it) }
+
+    println(palindromeNumber(122))
+
+
 
 
 }
