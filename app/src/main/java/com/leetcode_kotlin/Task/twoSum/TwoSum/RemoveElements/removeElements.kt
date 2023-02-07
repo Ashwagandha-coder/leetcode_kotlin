94e224435536ae158bbc250e9ec6b10a8194ecdd
Ashwagandha-coder <max.cacz@yandex.ru>
2023-02-08 03:59:45 +0500

    done task removeElements.kt and test

--- app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RemoveElements/removeElements.kt
+++ app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RemoveElements/removeElements.kt
@@ -2,17 +2,33 @@ package com.leetcode_kotlin.Task.twoSum.TwoSum.RemoveElements
 
 
 
-fun removeElements(nums: IntArray, value: Int) {
+fun removeElements(nums: IntArray, value: Int): Int {
 
+    var temp = 0
+    var count = 0
+    val range = nums.indices
 
+    for (i in range) {
+        if (nums[i] != value) {
+            temp = nums[i]
+            nums[count] = nums[i]
+            nums[i] = temp
+            count++
+        }
 
+    }
+    return count
 
 }
 
-fun customQuickSort() {
+fun main() {
 
+    val massive = intArrayOf(3,2,2,3)
+    val elem = 2
 
+    val result = removeElements(massive,elem)
 
+    println(result)
 
 
 }
