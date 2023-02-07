90a718d3519814a75c20f5456933854bbb961497
Ashwagandha-coder <max.cacz@yandex.ru>
2023-02-07 23:42:42 +0500

    task and test done - RomanToInteger.kt

--- app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RomanToInteger/RomanToInteger.kt
+++ app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RomanToInteger/RomanToInteger.kt
@@ -2,33 +2,28 @@ package com.leetcode_kotlin.Task.twoSum.TwoSum.RomanToInteger
 
 fun romanToInteger(s: String): Int {
 
-    val list = listOf("I","V","X","L","C","D","M")
-    val map = mapOf<String,Int>("I" to 1,"V" to 5,"X" to 10,"L" to 50,"C" to 100,"D" to 500,"M" to 1000)
+    var number = 0
+    var result = 0
+    val range = s.length - 1 downTo 0
 
-    var param = 0
-
-    for (i in map) {
-        if (i.key == s)
-            return i.value
-
-    }
-
-    for (j in s.length - 1..0) {
-
-        when(s[j]) {
-            'I' -> param = 1
-            'V' -> param = 5
-            'X' -> param = 10
-            'L' -> param = 50
-            'C' -> param = 100
-            'D' -> param = 500
-            'M' -> param = 1000
+    for (j in range) {
+        when (s[j]) {
+            'I' -> number = 1
+            'V' -> number = 5
+            'X' -> number = 10
+            'L' -> number = 50
+            'C' -> number = 100
+            'D' -> number = 500
+            'M' -> number = 1000
 
         }
 
+        if (4 * number < result) result -= number
+        else result += number
 
     }
 
-    return 0
+    return result
 
 }
+
--- app/src/test/java/com/leetcode_kotlin/RomanToInt/RomanToIntTest.kt
+++ app/src/test/java/com/leetcode_kotlin/RomanToInt/RomanToIntTest.kt
@@ -18,5 +18,50 @@ class RomanToIntTest {
 
     }
 
+    @Test
+    fun case_2() {
+
+        val value = "X"
+        val expected = 10
+        val actual = romanToInteger(value)
+
+        assertEquals(expected, actual)
+
+    }
+
+    @Test
+    fun case_3() {
+
+        val value = "IV"
+        val expected = 4
+        val actual = romanToInteger(value)
+
+        assertEquals(expected, actual)
+
+    }
+
+    @Test
+    fun case_4() {
+
+        val value = "IX"
+        val expected = 9
+        val actual = romanToInteger(value)
+
+        assertEquals(expected, actual)
+
+    }
+
+    @Test
+    fun case_5() {
+
+        val value = "III"
+        val expected = 3
+        val actual = romanToInteger(value)
+
+        assertEquals(expected, actual)
+
+
+    }
+
 
 }
