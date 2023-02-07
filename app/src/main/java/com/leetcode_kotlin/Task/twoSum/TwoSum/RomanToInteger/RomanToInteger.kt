f5630e6b66b65e2c90eddacc205019e29f53fad4
Ashwagandha-coder <max.cacz@yandex.ru>
2023-02-07 20:31:41 +0500

    [save] task RomanToInteger.kt

--- app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RomanToInteger/RomanToInteger.kt
+++ app/src/main/java/com/leetcode_kotlin/Task/twoSum/TwoSum/RomanToInteger/RomanToInteger.kt
@@ -5,10 +5,28 @@ fun romanToInteger(s: String): Int {
     val list = listOf("I","V","X","L","C","D","M")
     val map = mapOf<String,Int>("I" to 1,"V" to 5,"X" to 10,"L" to 50,"C" to 100,"D" to 500,"M" to 1000)
 
+    var param = 0
+
     for (i in map) {
-        if (i.key == s) {
+        if (i.key == s)
             return i.value
+
+    }
+
+    for (j in s.length - 1..0) {
+
+        when(s[j]) {
+            'I' -> param = 1
+            'V' -> param = 5
+            'X' -> param = 10
+            'L' -> param = 50
+            'C' -> param = 100
+            'D' -> param = 500
+            'M' -> param = 1000
+
         }
+
+
     }
 
     return 0
