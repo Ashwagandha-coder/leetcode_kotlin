b01c799b2920469f5cea399b8939ee2f8e3fb1fd
Ashwagandha-coder <max.cacz@yandex.ru>
2024-08-08 22:18:45 +0500

    add 3190

--- app/src/main/java/com/leetcode_kotlin/Algorithm.kt
+++ app/src/main/java/com/leetcode_kotlin/Algorithm.kt
@@ -1,5 +1,26 @@
 package com.leetcode_kotlin
 
-fun main() {
+/**
+ * Executing
+ */
 
+fun main() {
+    val arr = intArrayOf(1,2,50,4)
+    minimumOperations(arr)
 }
+
+
+/**
+ * 3190. Find Minimum Operations to Make All Elements Divisible by Three
+ */
+
+fun minimumOperations(nums: IntArray): Int {
+    var res = 0
+
+    for (n in nums) {
+        if (n % 3 > 0) res++
+    }
+
+    return res
+}
+
