81afd172bfc249bfe9203b0e2330326f4e1a6327
Ashwagandha-coder <max.cacz@yandex.ru>
2024-08-21 20:11:37 +0500

    repeat missing numbers

--- app/src/main/java/com/leetcode_kotlin/AlgoLeetcode-Repeat.kt
+++ app/src/main/java/com/leetcode_kotlin/AlgoLeetcode-Repeat.kt
@@ -3,3 +3,15 @@ package com.leetcode_kotlin
 fun main() {
     println("Hello World")
 }
+
+class Repeat
+
+
+fun Repeat.missingNumber(nums: IntArray): Int {
+    var sum = 0
+    var len = nums.size
+    for (i in 0..len - 1) sum += nums[i]
+    return len * (len + 1) / 2 - sum
+}
+
+
