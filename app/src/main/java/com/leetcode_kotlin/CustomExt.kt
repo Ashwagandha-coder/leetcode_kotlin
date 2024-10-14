0f444e946a0d55ff9d663d41ec98ab43f770bb09
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-14 23:10:31 +0500

    save data

--- app/src/main/java/com/leetcode_kotlin/CustomExt.kt
+++ app/src/main/java/com/leetcode_kotlin/CustomExt.kt
@@ -1,5 +1,7 @@
 package com.leetcode_kotlin
 
+import java.util.LinkedList
+
 fun IntArray.forEach(lambda: (Int) -> Unit) {
     for (elem in this) {
         lambda.invoke(elem)
@@ -13,7 +15,14 @@ fun sample() {
         if (it == 1) return@forEach
     }
     arr.count { it == 3 }
+    val linked = LinkedList<Int>(arr.toList())
+
 }
+
+/**
+ * Algo:
+ * 1. how to add array elements to linked list for cycle
+ */
 
 /**
  *  1. custom ext fun for collection (filter, map, forEach, indexed, etc...
