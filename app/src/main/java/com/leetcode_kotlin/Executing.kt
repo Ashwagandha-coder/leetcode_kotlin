4b2aa119f8bcfd6508e087b60c8ad2bf552060f6
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-14 12:21:15 +0500

    repeat linked cycle II

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -6,8 +6,9 @@ package com.leetcode_kotlin
 
 fun main() {
 
-    Solution.value.containsNearbyAlmostDuplicate(MyArrays.containsDuplicateIIIData(), 1, 3).apply {
-        println(this)
-    }
+    println("${detectCycle(LinkedList.makeLinkedList(intArrayOf(-1, -7, 7, -4, 19, 6, -9, -5, -2, -5)))?.`val`}")
+//    detectCycle(LinkedList.cycleLinkedListDataII()).let {
+//        println("${it?.`val`}")
+//    }
 
 }
--- app/src/main/java/com/leetcode_kotlin/TestingData.kt
+++ app/src/main/java/com/leetcode_kotlin/TestingData.kt
@@ -22,6 +22,29 @@ object LinkedList {
         return head
     }
 
+    fun cycleLinkedListDataII(): ListNode {
+        val head = ListNode(3)
+        val pos = ListNode(2)
+        head.next = ListNode(10)
+        head.next?.next = ListNode(0)
+        head.next?.next?.next = ListNode(-4)
+        head.next?.next?.next?.next = ListNode(20)
+        head.next?.next?.next?.next?.next = pos
+        return head
+    }
+
+    fun makeLinkedList(arr: IntArray): ListNode {
+        if (arr.isEmpty()) return ListNode(0)
+        var stub = ListNode(arr[0])
+        val head = stub
+        for (i in 1 until arr.size) {
+            val temp = ListNode(arr[i])
+            stub.next = temp
+            stub = stub.next!!
+        }
+        return head
+    }
+
 }
 
 object Node {
