df8e05a02210750c8507cbe84d997087c08f8552
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-18 20:32:48 +0500

    add 123 for prod variant

--- app/src/main/java/com/leetcode_kotlin/AlgorithmProdVariant.kt
+++ app/src/main/java/com/leetcode_kotlin/AlgorithmProdVariant.kt
@@ -1,2 +1,22 @@
 package com.leetcode_kotlin
 
+/**
+ * 123. Best Time to Buy and Sell Stock III
+ */
+
+class ProdVariant
+
+fun ProdVariant.maxProfitIII(prices: IntArray): Int {
+    var buy1 = Int.MAX_VALUE
+    var buy2 = Int.MAX_VALUE
+    var sell1 = Int.MIN_VALUE
+    var sell2 = Int.MIN_VALUE
+    prices.forEach {
+        buy1 = minOf(buy1, it)
+        sell1 = maxOf(sell1, it - buy1)
+        buy2 = minOf(buy2, it - sell1)
+        sell2 = maxOf(sell2, it - buy2)
+    }
+    return sell2
+}
+
