17ef0a814a6f014c32fb181b3575ca957df57575
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-23 18:22:29 +0500

    save change

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -9,6 +9,8 @@ fun main() {
 
     val head = intArrayOf(1, 2, 3, 4, 5).toListNode()
 
+    val arr = mutableListOf(1,2,3,4,5,6,7,8,9).filterIsInstance<Int>()
+
     TODO("Write custom ext function with generics and add rem note for repeat")
 
     reverseList(head).let { it?.printListNode() }
