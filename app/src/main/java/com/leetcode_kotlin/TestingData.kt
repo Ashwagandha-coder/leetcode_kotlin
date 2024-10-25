a7eb00688af77cc06abf11699da535f2ef191ae0
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-25 12:17:15 +0500

    add 322

--- app/src/main/java/com/leetcode_kotlin/TestingData.kt
+++ app/src/main/java/com/leetcode_kotlin/TestingData.kt
@@ -170,6 +170,23 @@ object DynamicProgramming {
     fun maxProfitIIData() = intArrayOf(7, 1, 5, 3, 6, 4)
 }
 
+class SolveSudoku {
+
+    fun makeSudoku(): Array<CharArray> = arrayOf(
+        charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+        charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+        charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+        charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+        charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+        charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+        charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+        charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+        charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+    )
+
+
+}
+
 
 
 
