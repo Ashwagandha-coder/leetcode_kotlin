1545a808760b1ce9ecfb726e5c9ede04d6095779
Ashwagandha-coder <max.cacz@yandex.ru>
2024-11-02 16:58:51 +0500

    add alt sol for 1957

--- app/src/main/java/com/leetcode_kotlin/AlternativeAlgorithmSolution.kt
+++ app/src/main/java/com/leetcode_kotlin/AlternativeAlgorithmSolution.kt
@@ -582,7 +582,50 @@ fun reverseListAlt(head: ListNode?): ListNode? {
     return newHead
 }
 
+/**
+ * 1957. Delete Characters to Make Fancy String
+ * Alt Solution with 2D dp array
+ */
 
+
+fun longestPalindrome(s: String): String {
+    val n = s.length
+    if (n < 2) return s
+
+    val dp = Array(n) { BooleanArray(n) }
+    var start = 0
+    var maxLength = 1
+
+
+    for (i in 0 until n) {
+        dp[i][i] = true
+    }
+
+
+    for (i in 0 until n - 1) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = true
+            start = i
+            maxLength = 2
+        }
+    }
+
+
+    for (len in 3..n) {
+        for (i in 0 until n - len + 1) {
+            val j = i + len - 1
+            if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = true
+                if (len > maxLength) {
+                    start = i
+                    maxLength = len
+                }
+            }
+        }
+    }
+
+    return s.substring(start, start + maxLength)
+}
 
 
 
