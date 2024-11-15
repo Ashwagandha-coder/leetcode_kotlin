cb3f7c56b79354081147f0314904f010f549a8d0
Ashwagandha-coder <max.cacz@yandex.ru>
2024-11-15 14:46:33 +0500

    add 263

--- app/src/main/java/com/leetcode_kotlin/AlgorithmLeetcode.kt
+++ app/src/main/java/com/leetcode_kotlin/AlgorithmLeetcode.kt
@@ -2722,6 +2722,33 @@ fun nthUglyNumber(n: Int): Int {
     return uglyNumbers[n - 1]
 }
 
+/**
+ * 263. Ugly Number
+ */
+
+fun isUgly(n: Int): Boolean {
+    if (n == 0) return false
+    var n = n
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2
+            continue
+        }
+        if (n % 3 == 0) {
+            n /= 3
+            continue
+        }
+        if (n % 5 == 0) {
+            n /= 5
+            continue
+        }
+        return false
+    }
+    return true
+}
+
+
+
 
 
 
