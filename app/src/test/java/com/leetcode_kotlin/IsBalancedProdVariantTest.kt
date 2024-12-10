490d9aeec3f4bda5acfe351e691f59ebfb719f41
Ashwagandha-coder <max.cacz@yandex.ru>
2024-12-10 12:28:42 +0500

    add test case's for balanced tree

--- app/src/test/java/com/leetcode_kotlin/IsBalancedProdVariantTest.kt
+++ app/src/test/java/com/leetcode_kotlin/IsBalancedProdVariantTest.kt
@@ -20,15 +20,36 @@ class IsBalancedProdVariantTest {
     }
 
     @Test
-    fun emptyTreeTest() {
-        val root = null
-
-        val call = isBalancedProdVariant(root)
-
-        assertEquals(true, call)
-
+    fun testIsBalanced_example2() {
+        val root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.left!!.left = TreeNode(3)
+        root.left!!.left!!.left = TreeNode(4)
+        root.left!!.left!!.left!!.left = TreeNode(5)
+        assertFalse(isBalanced(root))
     }
 
+    @Test
+    fun emptyTreeTest() {
+        val root = null
+        val call = isBalancedProdVariant(root)
+        assertEquals(true, call)
+    }
+
+
+    @Test
+    fun `singleNode`() {
+        val root = TreeNode(1)
+        assertTrue(isBalanced(root))
+    }
+
+    @Test
+    fun testIsBalanced_skewedTree() {
+        val root = TreeNode(1)
+        root.right = TreeNode(2)
+        root.right!!.right = TreeNode(3)
+        assertFalse(isBalanced(root))
+    }
 
 
 }
