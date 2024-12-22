2d6addc400cdc2522f3bf99d371e94f8bf1571d6
Ashwagandha-coder <max.cacz@yandex.ru>
2024-12-22 13:50:44 +0500

    change location of work tree

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -14,16 +14,6 @@ fun main() {
 
 }
 
-fun workTree() {
-    val root = TreeNode(1)
-    root.left = TreeNode(3)
-    root.right = TreeNode(2)
-    root.left!!.left = TreeNode(5)
-    root.left!!.right = TreeNode(3)
-    root.right!!.right = TreeNode(9)
-
-    widthOfBinaryTree(root).also { println(it) }
-}
 
 
 
