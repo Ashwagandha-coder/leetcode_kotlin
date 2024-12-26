dfca739451efe31db1cde88d9a405a252a294c29
Ashwagandha-coder <max.cacz@yandex.ru>
2024-12-26 14:26:38 +0500

    repeat largest value

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -10,9 +10,10 @@ fun main() {
 
     val array = intArrayOf(0, 0, 1, 2, 3, 5, 2, 3, 2, 1, 0)
 
-    peakIndexInMountainArray(array).let { println("$it") }
+    peakIndexInMountainArray(array).also { println("$it") }
 
 }
+
 
 
 
