b82bcb94e0a35e05d79c6a34cf80c97fa2c014cd
Ashwagandha-coder <max.cacz@yandex.ru>
2024-12-26 19:03:22 +0500

    add 34

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -8,15 +8,9 @@ package com.leetcode_kotlin
 
 fun main() {
 
+    val arr = intArrayOf(2, 2)
 
-    val root = TreeNode(1)
-    root.left = TreeNode(3)
-    root.right = TreeNode(2)
-    root.left!!.left = TreeNode(5)
-    root.left!!.right = TreeNode(3)
-    root.right!!.right = TreeNode(9)
-
-    largestValueAltSolution(root).also { println(it) }
+    searchRange(arr, 2).also { it.forEach { elem -> println(elem) } }
 
 }
 
