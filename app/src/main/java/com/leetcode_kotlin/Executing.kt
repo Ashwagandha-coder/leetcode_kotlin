875abc6e3461b640cf6a45ba2568c712efd1997d
Ashwagandha-coder <max.cacz@yandex.ru>
2024-12-28 19:54:01 +0500

    save change

--- app/src/main/java/com/leetcode_kotlin/Executing.kt
+++ app/src/main/java/com/leetcode_kotlin/Executing.kt
@@ -8,7 +8,7 @@ package com.leetcode_kotlin
 
 fun main() {
 
-    val arr = intArrayOf(1, 2, 3, 4, 5, 8, 9)
+    val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     hIndex(arr).also { println(it) }
 
