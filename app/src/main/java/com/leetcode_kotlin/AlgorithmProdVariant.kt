6b982bd8ac841ad228d13b2f5722e79d7ec4c35c
Ashwagandha-coder <max.cacz@yandex.ru>
2025-01-31 21:32:53 +0500

    add 1455

--- app/src/main/java/com/leetcode_kotlin/AlgorithmProdVariant.kt
+++ app/src/main/java/com/leetcode_kotlin/AlgorithmProdVariant.kt
@@ -856,3 +856,21 @@ fun Node?.forEachNode(block: (Node) -> Unit) {
     }
 }
 
+/**
+ * 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+ * Prod Variant
+ */
+
+
+fun isPrefixOfWordProdVariant(sentence: String, searchWord: String): Int {
+    val words = sentence.split(" ")
+    var counter = 1
+    for (word in words) {
+        when {
+            word.isEmpty() -> continue
+            word.length < searchWord.length -> counter++
+            else -> if (word.startsWith(searchWord)) return counter else counter++
+        }
+    }
+    return -1
+}
