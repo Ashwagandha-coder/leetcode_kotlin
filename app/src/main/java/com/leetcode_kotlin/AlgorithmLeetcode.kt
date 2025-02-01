e21b4d5aa7fd0b7c4b0c9584c8e9d64e65191110
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-01 18:18:04 +0500

    add 6

--- app/src/main/java/com/leetcode_kotlin/AlgorithmLeetcode.kt
+++ app/src/main/java/com/leetcode_kotlin/AlgorithmLeetcode.kt
@@ -5507,6 +5507,35 @@ fun areOccurrencesEqual(s: String): Boolean {
     return true
 }
 
+/**
+ * 6. Zigzag Conversion
+ */
+
+fun convert(s: String, numRows: Int): String {
+    if (numRows == 1 || numRows >= s.length) {
+        return s
+    }
+
+    val rows = List(numRows) { StringBuilder() }
+    var currentRow = 0
+    var goingDown = false
+
+    for (c in s) {
+        rows[currentRow].append(c)
+        if (currentRow == 0 || currentRow == numRows - 1) {
+            goingDown = !goingDown
+        }
+        currentRow += if (goingDown) 1 else -1
+    }
+
+    val result = StringBuilder()
+    for (row in rows) {
+        result.append(row)
+    }
+
+    return result.toString()
+}
+
 
 
 
