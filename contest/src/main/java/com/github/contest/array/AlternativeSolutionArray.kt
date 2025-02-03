48e2a272f46915a3498f8b481fd94beb5214951f
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-03 22:19:59 +0500

    add 2540 alt sol

---
+++ contest/src/main/java/com/github/contest/array/AlternativeSolutionArray.kt
@@ -0,0 +1,21 @@
+package com.github.contest.array
+
+/**
+ * 2540. Minimum Common Value
+ * Alternative Solution
+ */
+
+fun getCommonAlternativeSolution(nums1: IntArray, nums2: IntArray): Int {
+    var p1 = 0
+    var p2 = 0
+
+    while (p1 < nums1.size && p2 < nums2.size) {
+        when {
+            nums1[p1] == nums2[p2] -> return nums1[p1]
+            nums1[p1] < nums2[p2] -> p1++
+            else -> p2++
+        }
+    }
+
+    return -1
+}
