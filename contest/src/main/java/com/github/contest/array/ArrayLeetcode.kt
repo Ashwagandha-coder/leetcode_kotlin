31eb58e5634d73c3f882866100e1208a96389036
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-04 11:34:33 +0500

    add 1800

--- contest/src/main/java/com/github/contest/array/ArrayLeetcode.kt
+++ contest/src/main/java/com/github/contest/array/ArrayLeetcode.kt
@@ -1,3 +1,25 @@
 package com.github.contest.array
 
+/**
+ * 1800. Maximum Ascending Subarray Sum
+ */
 
+fun maxAscendingSum(nums: IntArray): Int {
+    if (nums.hasSingle()) return nums[0]
+    var maxAscending = 0
+    var sum = nums[0]
+    for (i in 1 until nums.size) {
+        if (nums[i] > nums[i - 1]) sum += nums[i]
+        else {
+            maxAscending = maxOf(maxAscending, sum)
+            sum = nums[i]
+        }
+    }
+    maxAscending = maxOf(maxAscending, sum)
+    return maxAscending
+}
+
+private fun IntArray.hasSingle(): Boolean = when {
+    this.size == 1 -> true
+    else -> false
+}
