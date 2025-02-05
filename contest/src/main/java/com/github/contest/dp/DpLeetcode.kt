675b868287892c4cbe478e2a95cd14fc0183200d
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-05 20:21:02 +0500

    add 70

--- contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
+++ contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
@@ -86,3 +86,18 @@ fun getRow(rowIndex: Int): List<Int> {
     }
     return curr
 }
+
+/**
+ * 70. Climbing Stairs
+ */
+
+fun climbStairs(n: Int): Int {
+    if (n <= 2) return n
+    val dp = IntArray(n)
+    dp[0] = 1
+    dp[1] = 2
+    for (i in 2 until n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n - 1]
+}
