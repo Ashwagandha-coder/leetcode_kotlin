a170b97739ab5c92774f796480541fc8777894f3
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-06 19:32:41 +0500

    add yet prod variant 70

--- contest/src/main/java/com/github/contest/dp/DpProdVariant.kt
+++ contest/src/main/java/com/github/contest/dp/DpProdVariant.kt
@@ -11,3 +11,9 @@ fun climbStairsProdVariant(n: Int): Int =
         prev2 to prev1 + prev2
     }.second
 
+private fun prodVariant(n: Int): Int = when {
+    n <= 2 -> n
+    else -> (3..n).fold(1 to 2) { (prev1, prev2), _ ->
+        prev2 to prev1 + prev2
+    }.second
+}
