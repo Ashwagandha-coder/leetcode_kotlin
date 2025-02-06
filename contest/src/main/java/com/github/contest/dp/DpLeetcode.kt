ad3998f635e32e2d1dd53be3d89022385e4dc27c
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-06 19:59:54 +0500

    add 516

--- contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
+++ contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
@@ -103,27 +103,28 @@ fun climbStairs(n: Int): Int {
 }
 
 /**
- *
+ * 516. Longest Palindromic Subsequence
  */
 
 
 fun longestPalindromeSubseq(s: String): Int {
+    if (s.hasSingle()) return 1
     val n = s.length
     val dp = Array(n) { IntArray(n) }
 
+    for (i in s.indices) dp[i][i] = 1
 
-    for (i in 0 until n) dp[i][i] = 1
-
-    for (len in 2 until n) {
-        for (i in 0 until n - len) {
-            val j = i + len - 1
-            if (s[i] == s[j]) {
-                dp[i][j] = if (i + 1 <= j - 1) dp[i + 1][j - 1] + 2 else 2
-            } else {
-                dp[i][j] = maxOf(dp[i + 1][j], dp[i][j - 1])
-            }
+    for (len in 2..n) {
+        for (i in 0..n - len) {
+            var j = (len - 1) + i
+            if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1] + 2
+            else dp[i][j] = maxOf(dp[i][j - 1], dp[i + 1][j])
         }
     }
+    return dp[0][n - 1]
+}
 
-    return dp[0].max()
+private fun String.hasSingle(): Boolean = when {
+    this.length == 1 -> true
+    else -> false
 }
