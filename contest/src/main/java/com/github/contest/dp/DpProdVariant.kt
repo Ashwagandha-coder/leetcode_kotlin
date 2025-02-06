0d5da0ddd0a027d6031b15e1bacac92bab5ef508
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-06 20:18:00 +0500

    add 516 prod variant

--- contest/src/main/java/com/github/contest/dp/DpProdVariant.kt
+++ contest/src/main/java/com/github/contest/dp/DpProdVariant.kt
@@ -17,3 +17,36 @@ private fun prodVariant(n: Int): Int = when {
         prev2 to prev1 + prev2
     }.second
 }
+
+/**
+ * 516. Longest Palindromic Subsequence
+ * Prod Variant
+ */
+
+
+fun longestPalindromicSubSequence(s: String): Int {
+    if (s.hasSingle()) return 1
+
+    val n = s.length
+    val dp = Array(n) { IntArray(n) }
+    s.forEachIndexed { index: Int, _ ->
+        dp[index][index] = 1
+    }
+
+    (2..n).forEach { len ->
+        (0..n - len).forEach { i ->
+            val j = i + len - 1
+            dp[i][j] = when {
+                s[i] == s[j] -> dp[i + 1][j - 1] + 2
+                else -> maxOf(dp[i][j - 1], dp[i + 1][j])
+            }
+        }
+    }
+
+    return dp[0][n - 1]
+}
+
+private fun String.hasSingle(): Boolean = when {
+    this.length == 1 -> true
+    else -> false
+}
