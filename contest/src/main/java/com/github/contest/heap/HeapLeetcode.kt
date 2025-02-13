5378dfc5a2abfae5e3b201d6b1c3bbb6c80e14e6
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-13 13:32:33 +0500

    add 3066

--- contest/src/main/java/com/github/contest/heap/HeapLeetcode.kt
+++ contest/src/main/java/com/github/contest/heap/HeapLeetcode.kt
@@ -1,2 +1,23 @@
 package com.github.contest.heap
 
+import java.util.PriorityQueue
+
+/**
+ * 3066. Minimum Operations to Exceed Threshold Value II
+ */
+
+fun minOperations(nums: IntArray, k: Int): Int {
+    var operation = 0
+    val pq = PriorityQueue<Long>()
+    for (num in nums) pq.offer(num.toLong())
+
+    while (pq.size > 1 && pq.peek() < k) {
+        val x = pq.poll()
+        val y = pq.poll()
+        val new = minOf(x, y) * 2 + maxOf(x, y)
+        pq.offer(new)
+        operation++
+    }
+
+    return operation
+}
