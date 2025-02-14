e242ff9c4e0343900c88991905b24a246dbf7de0
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-14 13:55:27 +0500

    add prod variant for 1352

--- contest/src/main/java/com/github/contest/Execute.kt
+++ contest/src/main/java/com/github/contest/Execute.kt
@@ -1,6 +1,6 @@
 package com.github.contest
 
-import com.github.contest.heap.halveArray
+import com.github.contest.design.ProductOfNumbersProdVariant
 
 
 /**
@@ -9,7 +9,25 @@ import com.github.contest.heap.halveArray
 
 fun main() {
 
-    halveArray(intArrayOf(7, 7, 7, 31, 2)).also { println(it) }
+    val productOfNumbers = ProductOfNumbersProdVariant()
+    productOfNumbers.add(3)
+    productOfNumbers.add(0)
+    productOfNumbers.add(2)
+    productOfNumbers.add(5)
+    productOfNumbers.add(4)
+    println(productOfNumbers.getProduct(2)) // Output: 20
+    println(productOfNumbers.getProduct(3)) // Output: 40
+    println(productOfNumbers.getProduct(4)) // Output: 0
+    productOfNumbers.add(0)
+    println(productOfNumbers.getProduct(2)) // Output: 0
+    productOfNumbers.add(3)
+    productOfNumbers.add(2)
+    productOfNumbers.add(5)
+    productOfNumbers.add(4)
+    println(productOfNumbers.getProduct(2)) // Output: 20
+    println(productOfNumbers.getProduct(3)) // Output: 40
+    println(productOfNumbers.getProduct(4)) // Output: 120
+    println(productOfNumbers.getProduct(5))
 
 
 }
