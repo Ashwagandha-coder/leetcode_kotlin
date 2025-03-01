e037ba7dd5b43321e907f029d11f73ea4cd6d065
Ashwagandha-coder <max.cacz@yandex.ru>
2025-03-01 09:28:55 +0000

    add 2460 alt sol

--- contest/src/main/java/com/github/contest/array/AlternativeSolutionArray.kt
+++ contest/src/main/java/com/github/contest/array/AlternativeSolutionArray.kt
@@ -40,3 +40,33 @@ fun sortArrayByParityIIAlternativeSolution(nums: IntArray): IntArray {
 
     return nums
 }
+
+/**
+ * 2460. Apply Operations to an Array
+ * Alternative Solution
+ */
+
+fun applyOperationsAlternativeSolution(nums: IntArray): IntArray {
+    for (i in 0 until nums.size - 1) {
+        if (nums[i] == nums[i + 1]) {
+            nums[i] *= 2
+            nums[i + 1] = 0
+        }
+    }
+
+    var insertPosition = 0
+    for (i in nums.indices) {
+        if (nums[i] != 0) {
+            nums[insertPosition] = nums[i]
+            insertPosition++
+        }
+    }
+
+    while (insertPosition < nums.size) {
+        nums[insertPosition] = 0
+        insertPosition++
+    }
+
+    return nums
+
+}
