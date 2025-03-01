f3b334ef5b1b5e304b2438b9551974b73ea6f352
Ashwagandha-coder <max.cacz@yandex.ru>
2025-03-01 09:00:22 +0000

    add 1143

--- contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
+++ contest/src/main/java/com/github/contest/dp/DpLeetcode.kt
@@ -351,3 +351,25 @@ fun maxAbsoluteSum(nums: IntArray): Int {
 
     return maxOf(maxSoFar, abs(minSoFar))
 }
+
+/**
+ * 1143. Longest Common Subsequence
+ */
+
+fun longestCommonSubsequence(text1: String, text2: String): Int {
+    val n = text1.length
+    val m = text2.length
+    val dp = Array(n + 1) { IntArray(m + 1) }
+
+    for (i in 1..n) {
+        for (j in 1..m) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = maxOf(dp[i][j - 1], dp[i - 1][j])
+            }
+        }
+    }
+
+    return dp[n][m]
+}
