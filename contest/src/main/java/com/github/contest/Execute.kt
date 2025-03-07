319200d5165bfc78a31629298ee3b53910e0664d
Ashwagandha-coder <max.cacz@yandex.ru>
2025-03-07 08:28:19 +0000

    add 2583

--- contest/src/main/java/com/github/contest/Execute.kt
+++ contest/src/main/java/com/github/contest/Execute.kt
@@ -1,7 +1,7 @@
 package com.github.contest
 
 
-import com.github.contest.hashTable.findMissingAndRepeatedValuesProdVariant
+import com.github.contest.math.closestPrimes
 import java.util.TreeMap
 
 
@@ -11,12 +11,7 @@ import java.util.TreeMap
 
 fun main() {
 
-    findMissingAndRepeatedValuesProdVariant(
-        arrayOf(
-            intArrayOf(1, 3),
-            intArrayOf(2, 2)
-        )
-    ).also { it.printArray() }
+    closestPrimes(19, 31).also { it.printArray() }
 }
 
 fun generateTesting() {
