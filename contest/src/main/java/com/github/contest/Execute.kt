f2b1a9791aeb12490f80885fa040cb847d945376
Ashwagandha-coder <max.cacz@yandex.ru>
2025-04-01 14:21:19 +0100

    add 2467

--- contest/src/main/java/com/github/contest/Execute.kt
+++ contest/src/main/java/com/github/contest/Execute.kt
@@ -1,7 +1,7 @@
 package com.github.contest
 
 
-import com.github.contest.dp.mostPointsDp
+import com.github.contest.graph.mostProfitablePath
 import java.util.TreeMap
 
 
@@ -11,10 +11,10 @@ import java.util.TreeMap
 
 fun main() {
 
-    val questions = arrayOf(intArrayOf(3, 2), intArrayOf(4, 3), intArrayOf(4, 4), intArrayOf(2, 5))
-
-    mostPointsDp(questions).also { println(it) }
+    val edges = arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(1, 3), intArrayOf(3, 4))
+    mostProfitablePath(edges, 3, intArrayOf(-2, 4, 2, -4, 6)).also { println(it) }
 }
+
 
 fun generateTesting() {
     val sequence = sequenceOf(3, 5, 6, 7, 7, 8, 8, 8, 9, 3)
