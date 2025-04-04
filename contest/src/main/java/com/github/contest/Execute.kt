5ec47efc7da1b5e0313b1808d5dfcf43822ad6d3
Ashwagandha-coder <max.cacz@yandex.ru>
2025-04-04 20:06:00 +0100

    add new file for strings and change last problem of strings

--- contest/src/main/java/com/github/contest/Execute.kt
+++ contest/src/main/java/com/github/contest/Execute.kt
@@ -12,7 +12,31 @@ import java.util.TreeMap
 
 fun main() {
 
-    shiftingLetters("abc", intArrayOf(3, 5, 9)).also { println(it) }
+    shiftingLetters(
+        "mkgfzkkuxownxvfvxasy",
+        intArrayOf(
+            505870226,
+            437526072,
+            266740649,
+            224336793,
+            532917782,
+            311122363,
+            567754492,
+            595798950,
+            81520022,
+            684110326,
+            137742843,
+            275267355,
+            856903962,
+            148291585,
+            919054234,
+            467541837,
+            622939912,
+            116899933,
+            983296461,
+            536563513
+        )
+    ).also { println(it) }
 }
 
 fun testing() {
--- contest/src/main/java/com/github/contest/strings/StringsLeetcode.kt
+++ contest/src/main/java/com/github/contest/strings/StringsLeetcode.kt
@@ -5,13 +5,18 @@ package com.github.contest.strings
  */
 
 fun shiftingLetters(str: String, shifts: IntArray): String {
-    val shifting = IntArray(shifts.size)
-    var sum = shifts.sum()
+    val shifting = LongArray(shifts.size)
+    var sum = 0L
     var res = ""
+
+    for (element in shifts) {
+        val num = element.toLong()
+        sum += num
+    }
 
     for (i in shifting.indices) {
         shifting[i] = sum
-        sum -= shifts[i]
+        sum -= shifts[i].toLong()
     }
 
     for (i in str.indices) {
@@ -23,13 +28,16 @@ fun shiftingLetters(str: String, shifts: IntArray): String {
     return res
 }
 
-private fun shiftLetter(char: Char, shift: Int): Char = when {
+private fun shiftLetter(char: Char, shift: Long): Char = when {
     char in 'a'..'z' -> {
         val base = 'a'.code
         val offset = char.code - base
         val shifted = (offset + shift) % 26
         (base + shifted).toChar()
     }
+
     else -> char
 }
+
+
 
