16f188197034ea96328f93e42413d8493c4d9bb1
Ashwagandha-coder <max.cacz@yandex.ru>
2025-04-05 08:25:05 +0100

    add prod variant 848

--- contest/src/main/java/com/github/contest/strings/StringsProdVariant.kt
+++ contest/src/main/java/com/github/contest/strings/StringsProdVariant.kt
@@ -1,2 +1,36 @@
 package com.github.contest.strings
 
+/**
+ * 848. Shifting Letters
+ */
+
+fun shiftingLetterProdVariant(str: String, shifts: IntArray): String {
+    var sum = shifts.toLongArray().sum()
+    return buildString {
+        str.forEachIndexed { index, letter ->
+            val new = letter.shiftLetter(sum)
+            append(new)
+            sum -= shifts[index]
+        }
+    }
+}
+
+private fun IntArray.toLongArray(): LongArray {
+    val new = LongArray(this.size)
+    for (i in indices) {
+        new[i] = this[i].toLong()
+    }
+
+    return new
+}
+
+private fun Char.shiftLetter(shift: Long): Char = when {
+    this in 'a'..'z' -> {
+        val base = 'a'.code
+        val offset = this.code - base
+        val shifted = (offset + shift) % 26
+        (base + shifted).toChar()
+    }
+
+    else -> this
+}
