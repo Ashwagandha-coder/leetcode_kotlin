fc8b82b8a4e5eaf13ed39e318c9674c453324891
Ashwagandha-coder <max.cacz@yandex.ru>
2025-04-27 18:06:11 +0100

    add 42

--- contest/src/main/java/com/github/contest/twoPointer/TwoPointerLeetCode.kt
+++ contest/src/main/java/com/github/contest/twoPointer/TwoPointerLeetCode.kt
@@ -20,3 +20,32 @@ fun compareVersion(version1: String, version2: String): Int {
     }
     return 0
 }
+
+/**
+ * 42. Trapping Rain Water
+ */
+
+
+fun trap(height: IntArray): Int {
+    if (height.isEmpty()) return 0
+
+    var left = 0
+    var right = height.size - 1
+    var leftMax = height[left]
+    var rightMax = height[right]
+    var water = 0
+
+    while (left < right) {
+        if (leftMax < rightMax) {
+            left++
+            leftMax = maxOf(leftMax, height[left])
+            water += leftMax - height[left]
+        } else {
+            right--
+            rightMax = maxOf(rightMax, height[right])
+            water += rightMax - height[right]
+        }
+    }
+
+    return water
+}
