33b4dd44a30c5ae9ec61d93875ee3ca135ad4a36
Ashwagandha-coder <max.cacz@yandex.ru>
2025-06-02 15:13:45 +0100

    add 900

--- contest/src/main/java/com/github/contest/Execute.kt
+++ contest/src/main/java/com/github/contest/Execute.kt
@@ -1,7 +1,7 @@
 package com.github.contest
 
 
-import com.github.contest.design.wrapper
+import com.github.contest.design.RLEIterator
 import com.github.contest.math.numberOfPowerfulInt
 import com.github.contest.slidingWindow.customStructure.rabinKarpMultiPattern
 import com.github.contest.slidingWindow.customStructure.slidingWindowClassic
@@ -16,7 +16,14 @@ import java.util.TreeMap
 
 fun main() {
 
-    wrapper()
+    val rleIterator = RLEIterator(intArrayOf(5, 2, 1, 22))
+    rleIterator.next(5).also { println(it) }
+    rleIterator.next(2).also { println(it) }
+    rleIterator.next(1).also { println(it) }
+}
+
+infix fun Int.myRange(to: Int): IntRange {
+    return this..to
 }
 
 
