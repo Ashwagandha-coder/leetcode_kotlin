e93ed4b8d9c3468dc097001fdb4c90274493cd0f
Ashwagandha-coder <max.cacz@yandex.ru>
2025-06-24 16:34:37 +0100

    add 461

--- contest/src/main/java/com/github/contest/bitManipulation/BitManipulationLeetcode.kt
+++ contest/src/main/java/com/github/contest/bitManipulation/BitManipulationLeetcode.kt
@@ -39,3 +39,19 @@ fun hammingWeight(n: Int): Int {
 
     return count
 }
+
+/**
+ * 461. Hamming Distance
+ */
+
+fun hammingDistance(x: Int, y: Int): Int {
+    var res = x xor y
+    var count = 0
+
+    while (res != 0) {
+        count += res and 1
+        res = res ushr 1
+    }
+
+    return count
+}
