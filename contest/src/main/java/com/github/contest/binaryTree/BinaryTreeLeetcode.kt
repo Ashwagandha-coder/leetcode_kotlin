6c6d10b36a918e9b1316523057b34d554888d6a2
Danil M <max.cacz@yandex.ru>
2025-09-15 01:28:14 -0400

    add 993 problem

--- contest/src/main/java/com/github/contest/binaryTree/BinaryTreeLeetcode.kt
+++ contest/src/main/java/com/github/contest/binaryTree/BinaryTreeLeetcode.kt
@@ -83,8 +83,55 @@ private fun dfs(root: TreeNode?): Pair<Int, Int> {
     if (root == null) return Pair(0, 0)
 
     val left = dfs(root?.left)
-    var right = dfs(root?.right)
+    val right = dfs(root?.right)
     val newTilt = abs(left.first - right.first)
 
     return Pair(root.`val` + left.first + right.first, newTilt + left.second + right.second)
+}
+
+/**
+ * 993. Cousins in Binary Tree
+ */
+
+fun isCousins(root: TreeNode?, x: Int, y: Int): Boolean {
+    root ?: return false
+    val queue = LinkedList<Pair<Int, TreeNode>>()
+    queue.offer(0 to root)
+
+    while (queue.isNotEmpty()) {
+        val size = queue.size
+        var parentX = 0
+        var parentY = 0
+        var isX = false
+        var isY = false
+
+        for (i in 0 until size) {
+            val (parentValue, node) = queue.poll()
+
+            if (node.`val` == x) {
+                parentX = parentValue
+                isX = true
+            }
+
+            if (node.`val` == y) {
+                parentY = parentValue
+                isY = true
+            }
+
+            if (isX && isY) {
+                if (parentX != parentY) return true
+            }
+
+            node.left?.let {
+                queue.offer(node.`val` to it)
+            }
+
+            node.right?.let {
+                queue.offer(node.`val` to it)
+            }
+
+        }
+    }
+
+    return false
 }
